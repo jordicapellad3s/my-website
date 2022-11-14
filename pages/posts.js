667672d2d8cb1596f414f5bb3bddf19777c4dbb7
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-11-14 17:53:26 +0100

    ✨ technical post page

--- pages/posts.js
+++ pages/posts.js
@@ -8,11 +8,21 @@ export default function posts() {
           ← Jordi Capellades
         </span>
       </Link>
-      <p className="p-2 mt-4 bg-neutral-200/50 rounded-xl text-neutral-600 font-inter">
-        Welcome to my blog. I don&apos;t know how often I will post here, but I
-        will try to do it as often as I can. I will post about my projects, my
-        thoughts, and my life. Likewise, I hope you enjoy it.
-      </p>
+      <div className="p-2 mt-4 bg-neutral-200/50 rounded-xl text-neutral-600 font-inter ">
+        <p className="">
+          Welcome to my blog. I don&apos;t know how often I will post here, but
+          I will try to do it as often as I can. I will post about my projects,
+          my thoughts, and my life. Likewise, I hope you enjoy it.
+        </p>
+        <p className="mt-2">
+          I also write blog posts about programming, but they&apos;re niche and
+          meant to be found when searching. Still, you can see a{" "}
+          <Link href="/technical-posts">
+            <span className="font-semibold underline">list of them here</span>
+          </Link>
+          .
+        </p>
+      </div>
     </div>
   );
 }
---
+++ pages/technical-posts.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+export default function technicalPosts() {
+  return (
+    <div>
+      <Link href="/" className="mb-3">
+        <span className="px-2 py-1 font-bold text-neutral-800 hover:underline">
+          ← Jordi Capellades
+        </span>
+      </Link>
+      <div className="p-2 mt-4 bg-neutral-200/50 rounded-xl text-neutral-600 font-inter ">
+        <p className="">
+          These are my blog posts on niche programming solutions, which are
+          probably only interesting if you&apos;re searching for a specific
+          solution.
+        </p>
+        <p className="mt-2 font-medium underline text-neutral-800">
+          <Link href="/posts">The rest of my blog posts are here.</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
