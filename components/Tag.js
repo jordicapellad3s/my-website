1292eefdbef2aabaa9c7ad775bfe69f1e6cc0af6
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-11-14 16:45:10 +0100

    💄 Fix padding of the Tag

--- components/Tag.js
+++ components/Tag.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Tag({ children, color }) {
   return (
     <span
-      className={`p-1 mx-1 font-semibold text-white bg-${color}-300 rounded-md`}
+      className={`px-2 mx-1 font-semibold text-white bg-${color}-300 rounded-md`}
     >
       {children}
     </span>
