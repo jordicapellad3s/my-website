2abdb41c0392a92449adc29638c63e08058572cd
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-11-15 12:27:31 +0100

    ✨ Create technical post page for the future

--- pages/technical-posts.js
+++ pages/technical-posts.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-export default function technicalPosts() {
+export default function TechnicalPosts() {
   return (
     <div>
       <Link href="/" className="mb-3">
