c5c4f0f1ff06a5fa48197ef610d45f46a5f38d4c
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-11-15 12:25:17 +0100

    ✨ Update personal component

--- components/Personal.js
+++ components/Personal.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Tag from "./Tag";
+import Tooltip from "./Tooltip";
 
 export default function Personal() {
   return (
@@ -8,28 +9,34 @@ export default function Personal() {
       <ul className="[&_li]:mt-3 ml-6 list-disc text-neutral-600 font-inter">
         <li>
           <p>
-            <Link href="/posts">
-              <span className="font-medium underline cursor-pointer">Blog</span>{" "}
-            </Link>
+            <span className="relative group w-max">
+              <Tooltip>Go to my blog!</Tooltip>
+              <Link href="/posts">
+                <span className="font-medium underline cursor-pointer">
+                  Blog
+                </span>{" "}
+              </Link>
+            </span>
             about my personal projects, life, and thoughts.
           </p>
         </li>
         <li>
           <p>
-            This personal website with <Tag color="neutral">Next</Tag>
+            This personal website with <Tag bg="bg-neutral-300">Next</Tag>
           </p>
         </li>
         <li>
           <p>
             <span className="font-medium">Spotify</span> clone with{" "}
-            <Tag color="neutral">Next</Tag> + <Tag color="blue">Tailwind</Tag>{" "}
+            <Tag bg="bg-neutral-300">Next</Tag> +{" "}
+            <Tag bg="bg-blue-300">Tailwind</Tag>{" "}
           </p>
         </li>
         <li>
           <p>
             <span className="font-medium">Vault</span>, a password manager for
-            myself with <Tag color="red">Laravel Breeze</Tag> +{" "}
-            <Tag color="cyan">React</Tag>.
+            myself with <Tag bg="bg-red-300">Laravel Breeze</Tag> +{" "}
+            <Tag bg="bg-cyan-300">React</Tag>
           </p>
         </li>
       </ul>
